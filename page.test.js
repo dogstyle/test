d531657a72126074fe25866f654c36ccad7aa55a
justinkempton <justin@kempton.com>
2014-01-09 15:44:43 -0800

    Adding code for code coverage

--- page.test.js
+++ page.test.js
@@ -1,27 +1,38 @@
+// using extend, puppy is instance, dog is prototype, and log is logging
 PAGE.extend(function(puppy, dog, log) {
 
 	var results = []
-	var allTestFiles = []
-	var onFinished = []
+	, allTestFiles = []
+	, allTestMaps = {}
+	, allTestMapsFlipped = {}
+	, onFinished = []
 
-	dog.test = {
+	var dtest = dog.test = {
 		results : results
-		, addTests : dog.addTests = addTests
 		, allTestFiles : dog.allTestFiles = allTestFiles
-		, runTest : dog.runTest = runTest
-		, runAllTests : dog.runAllTests = runAllTests
+		, allTestMaps : allTestMaps
+		, allTestMapsFlipped : allTestMapsFlipped
+
+		, addTests : function(path, func ){ return this }
+		, runTest : function(path){ return this }
+		, runSubTests : function(){ return this }
+		, runAllTests : function() { return this }
+		, onFinished : function(func) { onFinished.push(func) }
+		, addCoverage : function(callback) { return this }
+
 		, activeTests : []
 		, index : 0
-		, onFinished : function(func) {
-			onFinished.push(func)
-		}
 		, _testData : dog._testData = {}
 		, totalPass : 0
 		, totalFail : 0
+		, codeCoverage : undefined /* CodeCoverage(hashOfScriptsToCheck, test) */
+		, hasCodeCoverage : false
 	}
 
+	var ref = {}
+
 	// this overrides the PAGE.add method !
-	// modify add method to include path to test
+	// modifying add method to include path to test
 	dog.add = function(path, obj, test) {
 		if (typeof path === "undefined") return
 		var arr = path.split(".")
@@ -33,6 +44,12 @@ PAGE.extend(function(puppy, dog, log) {
 			// all arguments after path and object are considered test files
 			for(var x = 2; x < arguments.length; x++) {
 				allTestFiles.push(arguments[x])
+				var testMap = {
+					path : path
+					, obj : obj
+					, test : test
+				}
+				allTestMapsFlipped[arguments[x].toString()] = allTestMaps[path] = testMap
 			}
 		}
 		return puppy[group][item] = obj
@@ -44,49 +61,58 @@ PAGE.extend(function(puppy, dog, log) {
 	function finishedResults(results) {
 
 		for(var x = 0; x < results.length; x++) {
-			if (results[x].result) dog.test.totalPass += 1  
-			else dog.test.totalFail += 1
+			if (results[x].result) dtest.totalPass += 1  
+			else dtest.totalFail += 1
 		}
 
-		var temp = {
-			"Total Tests" :	{   count : Number(dog.test.totalPass + dog.test.totalFail) }
-			, "Total Passed" :	{  count : dog.test.totalPass }
-			, "Total Failed" : {  count : dog.test.totalFail }
+		var countAsync = 0
+			, countSync = 0
+			, countPassed = dtest.totalPass
+			, countFailed = dtest.totalFail
+			, countTotal = Number(dtest.totalPass + dtest.totalFail)
+			, countComplete = 0
+			, countPossible = 0
+
+		results.forEach(function(item, index, arr) {
+			if (item.type === "sync") countSync++
+			if (item.type === "async") countAsync++
+		})
+
+		var subCount = String("Σ Total Tests async [" + countAsync + "] -- sync [" + countSync + "]")
+
+		var temp = { }
+
+		temp[subCount] = { count : countTotal }
+		temp["✔ Tests Passed"] = { count : countPassed }
+		temp["✖ Tests Failed"] = { count : countFailed }
+
+		if (PAGE.exists("test.codeCoverage")) {
+			var countsCoverage = dtest.codeCoverage.getTotals()
+			temp["Functions found"] = { count : countsCoverage.countPossible }
+			temp["Functions covered"] = { count : countsCoverage.countComplete }
 		}
 
 		console.groupEnd()
 
 		console.table(temp, [ "count" ])
 
-		dog.test.totalPass = 0
-		dog.test.totalFail = 0
+
+		dtest.totalPass = 0
+		dtest.totalFail = 0
 
 	}
 
-	dog.test.onFinished(finishedResults)
+	dtest.onFinished(finishedResults)
 
-	var loadScript = function(pathToFile) {
-		var scriptId = clean(pathToFile)
-			, existingElm = document.getElementById(scriptId)
+	/* inherit from PAGE */
+	var loadScript = dog.loadScript
 
-		if (existingElm) {
-			existingElm.parentElement.removeChild(existingElm)
-		}
-
-		var fileref = document.createElement('script')
-		fileref.setAttribute("type","text/javascript")
-		fileref.setAttribute("src", pathToFile + "?" + (String(Math.random()).replace(/\./,""))) // randomize
-		fileref.setAttribute("id", scriptId)
-		document.getElementsByTagName("head")[0].appendChild(fileref)
-	}
-
-	function addTests(path, func /* (Constructor, Test, TestWaiter, comparer) */ ) {
+	dog.addTests = function(path, func /* (Constructor, Test, TestWaiter, comparer) */ ) {
 
 		var scout = {
 			syncTests : []
 			, asyncTests : []
 		}
-
 
 		function Test(testName, resultFunc) {
 
@@ -95,16 +121,19 @@ PAGE.extend(function(puppy, dog, log) {
 					name : testName
 					, result : resultFunc()
 					, test : resultFunc
-					, path : path
+					, construct : path
 					, func : func
+					, type : "sync"
 				}
 				results.push(result)
 				if (result.result) {
-					console.groupCollapsed(result.name, "\u2714 passed")
+					console.groupCollapsed("%c " + result.name + "%c \u2714", "color:gray; font-weight:normal;", "color:rgb(54, 231, 54)")
 					console.log(result)
 					console.groupEnd()
+
+
 				} else {
-					console.error(result.name, "\u2716 failed")
+					console.error(result.name + " \u2716 FAILED")
 					console.log(result)
 				}
 			})
@@ -124,9 +153,10 @@ PAGE.extend(function(puppy, dog, log) {
 			}
 
 			function call(result) {
+				result.type = "async"
 				results.push(result)
 				if (result.result) {
-					console.groupCollapsed(testName + ": ", result.name, "\u2714 passed")
+					console.groupCollapsed("%c " + testName + ": " + result.name + "%c \u2714", "color:gray; font-weight:normal;", "color:rgb(54, 231, 54)")
 					console.log(result)
 					console.groupEnd()
 				} else {
@@ -151,7 +181,7 @@ PAGE.extend(function(puppy, dog, log) {
 		function comparer(module, propertiesWithTypes) {
 			var truthy = true
 
-			typeof console === "object" && console.groupCollapsed("details")
+			typeof console === "object" && console.groupCollapsed("%ccomparer details", "color:gray; font-weight:normal")
 
 			if (!module) {
 				typeof console === "object" && console.error("length: ", x, "\u2716")
@@ -159,8 +189,6 @@ PAGE.extend(function(puppy, dog, log) {
 			}
 
 			for (var x in propertiesWithTypes) {
-				// if (typeof module[x] === "undefined") return false
-				
 				var run = true
 
 				if (!propertiesWithTypes[x].empty && module[x] === undefined) {
@@ -213,13 +241,86 @@ PAGE.extend(function(puppy, dog, log) {
 			return truthy
 		}
 
+		// this is here to help when a test group is finished, clear it up
 		var interval = setInterval(function() {
 			if (!scout.asyncTests.length) {
 				clearInterval(interval)
+
+				if (PAGE.exists("test.codeCoverage")) {
+					;(function() {
+						var countsCoverage = dtest.codeCoverage.getTotals()
+						var temp = {}
+						var lastTest = dtest.lastTest
+						if (!lastTest) { return alert("oop") }
+
+						var lastTestMap = dtest.allTestMapsFlipped[lastTest]
+						var lastConstructorCounts = dtest.codeCoverage.getByPath( lastTestMap.path )
+
+						temp["functions found"] = lastConstructorCounts ? { count : lastConstructorCounts.totalPoints } : 0
+						temp["functions tested"] = lastConstructorCounts ? { count : lastConstructorCounts.hitsCount } : 0
+						temp["functions missed"] = lastConstructorCounts ? { count : lastConstructorCounts.hitsMissed } : 0
+						console.groupEnd()
+						console.group("Code Coverage")
+						console.table(temp, [ "count" ])
+
+						lastTestMap.coverage = lastConstructorCounts
+
+						console.group("%c Coverage Info", "font-weight:normal; color:#aaa;")
+						// console.log(lastTestMap.coverage)
+						// console.log("All Test Data")
+						// console.log(lastTestMap)
+
+
+						;(function() {
+							var tempMissesList = {}
+
+							if (!lastTestMap.coverage) return
+
+							var misses = lastTestMap.coverage.misses
+
+							for(var x in misses) {
+								tempMissesList[x] = {
+									lineNumber : misses[x].loc.start.line
+									, column : misses[x].loc.start.column
+									, name : misses[x].name
+								}
+							}
+
+							console.group("Misses")
+							console.table(tempMissesList, ["name", "lineNumber", "column"])
+							console.groupEnd()
+
+						}())
+
+						;(function() {
+							var tempHitsList = {}
+
+							if (!lastTestMap.coverage) return
+
+							var hits = lastTestMap.coverage.hits
+
+							for(var x in hits) {
+								tempHitsList[x] = {
+									lineNumber : hits[x].lineNumber
+									, name : hits[x].name
+								}
+							}
+
+							console.groupCollapsed("Hits")
+							console.table(tempHitsList, ["name", "lineNumber"])
+							console.groupEnd()
+
+						}())
+
+						console.groupEnd()
+						console.groupEnd()
+					}())
+				}
+
 				console.groupEnd()
 
-				if (dog.test.activeTests.length) {
-					runTest(dog.test.activeTests.shift())
+				if (dtest.activeTests.length) {
+					subRunTest(dtest.activeTests.shift())
 				} else {
 					for(var x = 0; x < onFinished.length; x++) {
 						onFinished[x](results)
@@ -241,17 +342,64 @@ PAGE.extend(function(puppy, dog, log) {
 		dog.allTestFiles = dog.allTestFiles.concat(arr)
 	}
 
-	function runAllTests() {
-		dog.test.results.length = 0
-		dog.test.activeTests = dog.test.activeTests.concat( dog.allTestFiles )
-		console.group("%cPAGE%c☃%cTESTING","font-size:42px;", "font-size:42px; color:hsl(0,0%, 84%); font-weight:normal;", "font-size:42px;")
-		runTest( dog.test.activeTests.shift() )
-		return "running all tests, build created 12-24-2013"
+	/* this is to add coverage stats for all tests */
+	dog.addCoverage = function(callback) {
+
+		if (dtest.hasCodeCoverage) {
+			typeof callback === "function" && callback(dtest)
+			return dtest
+		}
+
+		/* load it if it's not there */
+		PAGE.exists("Utils.CodeCoverage") ? "" : loadScript("/Scripts/testUtils/page.test.codeCoverage.js")
+
+		// Main code coverage
+		PAGE.wait("Utils.CodeCoverage", function(CodeCoverage) {
+
+			dtest.codeCoverage = CodeCoverage(PAGE.Constructors, dtest)
+			.start(function() {
+				typeof callback === "function" && callback(dtest)
+			})
+
+			dtest.hasCodeCoverage = true
+
+		})
+
+		return dtest
 	}
 
-	function runTest(path) {
-		console.group(path)
+	dog.runSubTests = function() {
+		dtest.results.length = 0
+		dtest.activeTests = dtest.activeTests.concat( dog.allTestFiles )
+		console.group("%cPAGE%c♗%cTEST%c〄k","font-size:42px; color:rgb(229, 229, 209); font-weight:normal;", "font-size:42px; color:rgb(233, 227, 80); font-weight:normal;", "font-size:42px; color:rgb(229, 229, 209); font-weight:normal;", "font-size:16px; color:black; font-weight:normal;")
+
+		console.log("PAGE.test : Version 1 : DEC 30 2013")
+		subRunTest( dtest.activeTests.shift() )
+
+		return "running all tests, build created 12-24-2013"
+}
+
+	dog.runAllTests = function() {
+		dog.addCoverage( dog.runSubTests )
+		return "Release the Kraken!"
+	}
+
+	function subRunTest(path) {
+		dtest.lastTest = path
 		loadScript(path)
+		console.group(path)
+		console.group("tests")
+	}
+
+	dog.runTest = function(path) {
+		dtest.lastTest = path
+		dog.addCoverage( function() {
+			dtest.results.length = 0
+			loadScript(path)
+			console.group(path)
+			console.group("tests")
+		})
+		return "Release the Kraken!"
 	}
 
 })
